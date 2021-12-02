b47a9dc30e356e52a98a72c2baf8d45d1c7b7f32
Erfan Motallebi <Erfan.Motallebi1990@gmail.com>
2021-12-03 00:13:19 +0330

    added some features to test the resolvers

---
+++ index.ts
@@ -0,0 +1,66 @@
+import { createConnection } from "typeorm";
+import "reflect-metadata";
+import { ApolloServer } from "apollo-server-express";
+import express, { Express, Request } from "express";
+import { buildSchema } from "type-graphql";
+import expressSession from "express-session";
+import connectRedis from "connect-redis";
+import { nanoid } from "nanoid";
+import { Redis } from "./src/redis";
+import cors, { CorsOptions } from "cors";
+import { ILoginContextReuqest } from "./src/module/users/types";
+import { createSchema } from "./src/module/utils/createSchema";
+
+async function bootStrap() {
+  await createConnection();
+
+  interface ContextType extends ILoginContextReuqest {}
+
+  const schema = await createSchema();
+
+  const server = new ApolloServer({
+    schema,
+    context: ({ req, res }: ILoginContextReuqest) => ({ req, res }),
+  });
+
+  await server.start();
+  const app: Express = express();
+  const RedisStore = connectRedis(expressSession as any);
+
+  const corOptions: CorsOptions = {
+    methods: ["POST", "GET"],
+    origin: [
+      "https://studio.apollographql.com",
+      "http://localhost:4000/graphql",
+    ],
+    credentials: true,
+  };
+  app.use(cors(corOptions));
+  app.use(cors());
+  app.use(
+    expressSession({
+      name: "QID",
+      store: new RedisStore({ client: Redis }) as any,
+      secret: "abcdef",
+      resave: false,
+      saveUninitialized: false,
+      genid(req: Request) {
+        return nanoid();
+      },
+      cookie: {
+        secure: true,
+        sameSite: "none",
+        httpOnly: true,
+        maxAge: 1000 * 60 * 60,
+      },
+    })
+  );
+
+  server.applyMiddleware({ app, cors: false });
+
+  app.listen(4000, "localhost", async () => {
+    console.log("Server Ready at http://localhost:4000");
+  });
+}
+
+bootStrap();
